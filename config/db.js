7e39eedcd6f3db34e975433ebcef1fdabee0c713
Yanick <tyankee15@gmail.com>
2021-09-30 13:43:01 -0300

    Add db.js and update index.js to connect app to MongoDB server

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Replace this with your MONGOURI.
+const MONGOURI = "mongodb+srv://dbUser:<Iceboy323@#>@cluster0.mtiia.mongodb.net/Cluster0?retryWrites=true&w=majority";
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
